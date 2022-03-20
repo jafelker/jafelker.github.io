6ea1963ac95a460f5e4264b42dcd459578f18220
Joshua Felker <jafelker@umich.edu>
2022-03-20 17:54:34 -0400

    Copied over stuff from old site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card'
+import { MatTabsModule } from '@angular/material/tabs';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { InfoComponent } from './info/info.component';
+import { ProjectsComponent } from './projects/projects.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    InfoComponent,
+    ProjectsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatTabsModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent {
+  public projects = [
+    {
+      title: 'Unstable Towers',
+      link: 'https://adamk33n3r.itch.io/unstable-towers',
+      repo: 'https://github.com/adamk33n3r/LD49',
+      img: 'unstable-towers.png',
+      description: 'Ludem Dare 49 was the first game jam I participated in. I entered into the jam with one other person.'
+    },
+    {
+      title: 'UNO&reg; Online!',
+      link: '/uno',
+      repo: 'http://github.com/jafelker/uno',
+      img: 'uno.png',
+      description: 'This is a web app that I designed and implemented with a team of four for my UI class in college.'
+    },
+    {
+      title: 'Block Breaker',
+      link: '/BlockBreaker',
+      repo: 'http://github.com/jafelker/BlockBreaker',
+      img: 'BlockBreaker.png',
+      description: 'This is my first game using Unity.'
+    },
+  ];
+}
